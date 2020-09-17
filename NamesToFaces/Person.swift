b5c00ce8d7557e42975d43ff2f5b391d5795c19a
Marina Khort <maryna.khort@gmail.com>
2020-09-18 00:09:17 +0300

    Have added functionality about saving pictures to the directory

---
+++ NamesToFaces/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Marina Khort on 18.09.2020.
+//  Copyright © 2020 Marina Khort. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
