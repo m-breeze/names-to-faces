226f3bea36e0108be61d382c57cfdf5074f0007e
Marina Khort <maryna.khort@gmail.com>
2020-10-13 01:52:51 +0300

    Adds different filters

--- Photofilter/ViewController.swift
+++ Photofilter/ViewController.swift
@@ -6,20 +6,26 @@
 //  Copyright © 2020 Marina Khort. All rights reserved.
 //
 
+import CoreImage
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	@IBOutlet var imageView: UIImageView!
-	@IBOutlet var slider: UISlider!
-	var currentImage: UIImage!
+	@IBOutlet var intensity: UISlider!
 	
+	var currentImage: UIImage!
+	var context: CIContext!
+	var currentFilter: CIFilter!
 	
 	override func viewDidLoad() {
 			super.viewDidLoad()
 	//import a photo from the user's photo library
 		title = "Photo filter"
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+		
+		context = CIContext()
+		currentFilter = CIFilter(name: "CISepiaTone")
 		}
 	
 	@objc func importPicture() {
@@ -30,27 +36,77 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 	}
 	
 	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-		guard let image = info[.editedImage] as? UIImage else {return}
+		guard let image = info[.editedImage] as? UIImage else { return }
 		dismiss(animated: true)
 		
 		//to have a copy of pic what is originally imported
 		currentImage = image
-	}
-	
-	@IBAction func intensityChanged(_ sender: UISlider) {
 		
-	}
-	
-	@IBAction func changeFilter(_ sender: UIButton) {
+		let beginImage = CIImage(image: currentImage)
+		currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
 		
+		applyProcessing()
 	}
 	
 	
-	@IBAction func save(_ sender: UIButton) {
+	@IBAction func intensityChanged(_ sender: Any) {
+		applyProcessing()
+	}
+	
+
+	@IBAction func changeFilter(_ sender: Any) {
+		let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+		ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+		ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		present(ac,animated: true)
+	}
+	
+	@IBAction func save(_ sender: Any) {
+	}
+	
+
+	func setFilter(action: UIAlertAction) {
+		// make sure we have a valid image before continuing
+		guard currentImage != nil else { return }
+		// safely read the alert action's title
+		guard let actionTitle = action.title else { return }
+		currentFilter = CIFilter(name: actionTitle)
 		
+		let beginImage = CIImage(image: currentImage)
+		currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+		
+		applyProcessing()
 	}
 	
-	
+	func applyProcessing() {
+		let inputKeys = currentFilter.inputKeys
+		if inputKeys.contains(kCIInputIntensityKey) {
+			currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+		}
+		if inputKeys.contains(kCIInputRadiusKey) {
+			currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+		}
+		if inputKeys.contains(kCIInputScaleKey) {
+			currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+		}
+		if inputKeys.contains(kCIInputCenterKey) {
+			currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+		}
+		
+//		guard let image = currentFilter.outputImage else { return }
+		
+		
+		if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+			let processedImage = UIImage(cgImage: cgimg)
+			imageView.image = processedImage
+		}
+	}
 
 
 }
